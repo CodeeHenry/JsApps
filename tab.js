431be86b25d12a8c3a10b9fa772b67737f3a6a99
Henry Kibocha <47037683+CodeeHenry@users.noreply.github.com>
2019-06-25 17:58:59 +0300

    Add files via upload

---
+++ tab.js
@@ -0,0 +1,28 @@
+var tabButtons = document.querySelectorAll(".tabContainer .buttonContainer button");
+var tabPanels = document.querySelectorAll(".tabContainer .tabPanel");
+
+function showPanel(panelIndex, colorCode){
+
+	tabButtons.forEach(function(node){
+
+		node.style.backgroundColor = "";
+		node.style.color = "";
+	});
+
+	tabButtons[panelIndex].style.backgroundColor = colorCode;
+	tabButtons[panelIndex].style.backgroundColor = "white";
+	tabPanels.forEach(function(node){
+
+		// node.style.backgroundColor = "";
+		// node.style.color = "";
+
+		node.style.display = "none";
+
+	});
+
+	tabPanels[panelIndex].style.display = "block";
+	tabPanels[panelIndex].style.backgroundColor = colorCode;
+
+}
+
+showPanel(0, '#f44336');
